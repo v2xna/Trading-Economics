2aff224e67286b406da0d0cd82f6f6a347027ec5
vithu200 <ecko@live.ca>
2023-10-01 14:05:49 -0400

    added labels on top of the bars for the chart

--- chart.js
+++ chart.js
@@ -1,9 +1,12 @@
 const ctx = document.getElementById("myChart");
 let dataValues = [6.3, 5.1, 4.1, 3.9, 2.7, 1.4, -0.2, -0.6, -0.7, -0.9, -0.9];
 
-let backgroundColors = dataValues.map(function (value) {
-  return value >= 0 ? "#0564b1" : "#eedb89";
-});
+let backgroundColors = dataValues.map((value) =>
+  value >= 0 ? "#0564b1" : "#eedb89"
+);
+
+let anchors = dataValues.map((value) => (value >= 0 ? "end" : "start"));
+let aligns = dataValues.map((value) => (value >= 0 ? "top" : "bottom"));
 
 new Chart(ctx, {
   type: "bar",
@@ -28,9 +31,14 @@ new Chart(ctx, {
         data: dataValues,
         borderWidth: 1,
         backgroundColor: backgroundColors,
+        datalabels: {
+          anchor: anchors,
+          align: aligns,
+        },
       },
     ],
   },
+  plugins: [ChartDataLabels],
   options: {
     scales: {
       y: {
@@ -41,7 +49,7 @@ new Chart(ctx, {
         ticks: {
           stepSize: 2,
           callback: function (value) {
-            return value.toFixed(2) + "%";
+            return value.toFixed(2) + " %";
           },
         },
       },
