a5e511ea512aeb1cc047cb8dbf82823bda831dcd
vithu200 <ecko@live.ca>
2023-10-06 12:44:01 -0400

    navbar and dropdown changes

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ document.addEventListener("click", (e) => {
     dropdown.classList.remove("active");
   });
 });
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
