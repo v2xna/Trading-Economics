6477fdc5c7566e9bb1bc93edce3e4a4e1a1c3659
vithu200 <ecko@live.ca>
2023-10-14 12:16:30 -0400

    dropdown opens when clicked

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const btnDropdown = document.querySelector("#dropdown-btn");
+const dropdownMenu = document.querySelector(".dropdown-menu");
 
+btnDropdown.addEventListener("click", () => {
+  dropdownMenu.classList.add("open");
+});
