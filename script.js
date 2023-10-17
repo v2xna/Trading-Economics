6f89b8861727e4d783c75fc2bbc67812c1826f68
vithu200 <ecko@live.ca>
2023-10-16 20:09:28 -0400

    hides dropdown if user clicks outside of dropdown

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const btnDropdown = document.querySelector("#dropdown-btn");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
-btnDropdown.addEventListener("click", () => {
+btnDropdown.addEventListener("click", (e) => {
+  e.stopPropagation();
   dropdownMenu.classList.add("open");
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target === btnDropdown) return;
+  dropdownMenu.classList.remove("open");
+});
+
+dropdownMenu.addEventListener("click", (e) => {
+  e.stopPropagation();
 });
