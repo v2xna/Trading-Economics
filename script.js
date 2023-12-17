0249b4542deb72e9476bb5d6a78dc8941f393825
Vithursan <ecko@live.ca>
2023-12-17 18:22:14 -0500

    close dropdown menu when user clicks outside of dropdown menu or dropdown button

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const btnDropDown = document.querySelectorAll(".dropdown-btn");
 const menuDropdown = document.querySelectorAll(".dropdown-menu");
-const overlay = document.querySelector(".overlay");
 
 btnDropDown.forEach(function (btn, i) {
-  btn.addEventListener("click", function () {
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
     menuDropdown[i].classList.toggle("hidden");
-    overlay.classList.toggle("hidden");
   });
+});
+
+document.body.addEventListener("click", function (e) {
+  const isDropdownButton = e.target.matches(".dropdown-btn");
+  const isDropdownMenu = e.target.matches(".dropdown-menu");
+
+  if (!isDropdownButton && !isDropdownMenu) {
+    menuDropdown.forEach(function (menu) {
+      menu.classList.add("hidden");
+    });
+  }
 });
